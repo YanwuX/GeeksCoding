156c147925c6f6b726f565743d6ac8d075c0994c
YanwuX <yxu73@wisc.edu>
2016-07-09 16:49:04 -0700

    LRU

---
+++ geeksCoding/src/leetCode/LRU.java
@@ -0,0 +1,44 @@
+package leetCode;
+
+import java.util.ArrayList;
+import java.util.HashSet;
+import java.util.LinkedHashMap;
+import java.util.Map;
+
+public class LRU {
+	
+    private LinkedHashMap<Integer, Integer> map;
+    private final int CAPACITY;
+    
+    public LRU(int capacity) {
+        CAPACITY = capacity;
+        map = new LinkedHashMap<Integer, Integer>(capacity, 1.0f, true){
+        	@Override
+            protected boolean removeEldestEntry(Map.Entry eldest) {
+                return size() > CAPACITY;
+            }
+        };
+    }
+    
+    public int get(int key) {
+        return map.getOrDefault(key, -1);
+    }
+    
+    public void add(int key, int value) {
+        map.put(key, value);
+    }
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+		LRU lru = new LRU(5);
+		lru.add(1, 1);
+		lru.add(2, 2);
+		lru.add(3, 3);
+		lru.add(4, 4);
+		lru.add(5, 5);
+		lru.add(6, 6);
+		lru.add(5, 3);
+		lru.add(5, 5);
+	}
+
+}
