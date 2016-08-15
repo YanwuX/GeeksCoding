3066522838fd180f8f92272edc209b3849629190
YanwuX <yxu73@wisc.edu>
2016-08-14 23:16:00 -0700

    sample Jaxb

---
+++ geeksCoding/src/sampleJAXB/TestData.java
@@ -0,0 +1,28 @@
+package sampleJAXB;
+
+public class TestData {
+	
+	private String tittle;
+	private String book;
+	private int count;
+	
+	public String getTittle() {
+		return tittle;
+	}
+	public void setTittle(String tittle) {
+		this.tittle = tittle;
+	}
+	public String getBook() {
+		return book;
+	}
+	public void setBook(String book) {
+		this.book = book;
+	}
+	public int getCount() {
+		return count;
+	}
+	public void setCount(int count) {
+		this.count = count;
+	}
+
+}
