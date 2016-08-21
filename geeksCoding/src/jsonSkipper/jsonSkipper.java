e2634c401331e7a302bf134e0a9457934370622a
YanwuX <yxu73@wisc.edu>
2016-08-21 14:10:36 -0700

    Geeks update

---
+++ geeksCoding/src/jsonSkipper/jsonSkipper.java
@@ -0,0 +1,12 @@
+package jsonSkipper;
+
+import java.io.StringReader;
+
+public class jsonSkipper {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		JsonParser parser = Json.createParser(new StringReader("[]"));
+	}
+
+}
