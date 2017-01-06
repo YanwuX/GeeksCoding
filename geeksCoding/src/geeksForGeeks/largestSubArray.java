2ced1545efd21042cb4483dc6dc7bbf9cf7a9ffa
言午 <yxu73@wisc.edu>
2017-01-06 02:36:54 -0800

    Geeks Update

---
+++ geeksCoding/src/geeksForGeeks/largestSubArray.java
@@ -0,0 +1,26 @@
+package geeksForGeeks;
+
+public class largestSubArray {
+
+	public static int maxSubArray(int[] A) {
+
+		if (A == null || A.length == 0) {
+			return 0;
+		}
+
+		int max = Integer.MIN_VALUE, sum = 0;
+
+		for (int i = 0; i < A.length; ++i) {
+			sum += A[i];
+			max = Math.max(max, sum);
+			sum = Math.max(sum, 0);
+		}
+		return max;
+	}
+
+	public static void main(String[] args) {
+		int[] arr = new int[] {4, 3, -5, 0, 6, -8, 12, 3, -9, 2, 5, 8, -3};
+		System.out.println(maxSubArray(arr));
+	}
+
+}
